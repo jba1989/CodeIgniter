b2312c8ecb74781e71f9c769f14f3f5175558d2e
jba1989 <jba1989@gmail.com>
2018-10-13 01:28:07 +0800

    完成氣象資料爬蟲和weather_template

--- application/controllers/api/Weather.php
+++ application/controllers/api/Weather.php
@@ -10,6 +10,19 @@ class weather extends CI_Controller
 {
     public function index()
     {
-        echo 'in api';
+        $url = 'https://tinyurl.com/y7y54gms';
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch,  CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+
+        $result = curl_exec($ch);
+
+        curl_close($ch);
+
+        $resultArr = json_decode($result, TRUE);
+        $this->load->view('weather/weather', array('data' => $resultArr['records']['location']));
     }
 }
---
+++ application/views/weather/weather.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lonelygod
+ * Date: 2018/10/10
+ * Time: 下午 9:23
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<head>
+    <style>
+        table {
+            margin:auto;
+        }
+
+        table, td, th {
+            border:solid 1px black;
+        }
+
+        td {
+            padding: 0.5em 1.5em 0.5em 1.5em;
+            text-align: center;
+        }
+
+        th {
+            background-color: RGB(100,100,100);
+            color: white;
+        }
+
+        tr.type1 {
+            background-color: RGB(235,235,235);
+        }
+
+    </style>
+</head>
+<body>
+    <table>
+        <caption>三十六小時天氣預報</caption>
+        <tr>
+            <th>地區</th>
+            <th>時間</th>
+            <th>天氣狀況</th>
+            <th>降雨機率</th>
+            <th>溫度</th>
+        </tr>
+        <?php foreach ($data as $num => $location) :?>
+            <tr class="type<?php echo ($num % 2);?>">
+                <td rowspan="3"><?php echo $location['locationName']; ?></td>
+                <td><?php echo $location['weatherElement'][0]['time'][0]['startTime'] . '~' . $location['weatherElement'][0]['time'][0]['endTime']; ?></td>
+                <td><?php echo $location['weatherElement'][0]['time'][0]['parameter']['parameterName']; ?></td>
+                <td><?php echo $location['weatherElement'][1]['time'][0]['parameter']['parameterName'] . '%'; ?></td>
+                <td><?php echo $location['weatherElement'][2]['time'][0]['parameter']['parameterName'] . '度'; ?></td>
+            <tr class="type<?php echo ($num % 2);?>">
+                <td><?php echo $location['weatherElement'][0]['time'][1]['startTime'] . '~' . $location['weatherElement'][0]['time'][1]['endTime']; ?></td>
+                <td><?php echo $location['weatherElement'][0]['time'][1]['parameter']['parameterName']; ?></td>
+                <td><?php echo $location['weatherElement'][1]['time'][1]['parameter']['parameterName'] . '%'; ?></td>
+                <td><?php echo $location['weatherElement'][2]['time'][1]['parameter']['parameterName'] . '度'; ?></td>
+            </tr>
+            <tr class="type<?php echo ($num % 2);?>">
+                <td><?php echo $location['weatherElement'][0]['time'][2]['startTime'] . '~' . $location['weatherElement'][0]['time'][2]['endTime']; ?></td>
+                <td><?php echo $location['weatherElement'][0]['time'][2]['parameter']['parameterName']; ?></td>
+                <td><?php echo $location['weatherElement'][1]['time'][2]['parameter']['parameterName'] . '%'; ?></td>
+                <td><?php echo $location['weatherElement'][2]['time'][2]['parameter']['parameterName'] . '度'; ?></td>
+            </tr>
+        <?php endforeach ?>
+    </table>
+<body>
